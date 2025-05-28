badc0440dd5566b46c67aab80f033c2d56d27316
chiennq <chiennq1910@gmail.com>
2025-05-28 23:33:35 +0700

    add prisma schema

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,8 +1,7 @@
-import { plainToInstance } from 'class-transformer'
-import { IsString, validateSync } from 'class-validator'
+import { config } from 'dotenv'
 import fs from 'fs'
 import path from 'path'
-import { config } from 'dotenv'
+import { z } from 'zod'
 
 config({
   path: '.env',
@@ -13,36 +12,21 @@ if (!fs.existsSync(path.resolve('.env'))) {
   process.exit(1)
 }
 
-class ConfigSchema {
-  @IsString()
-  DATABASE_URL: string
-  @IsString()
-  ACCESS_TOKEN_SECRET: string
-  @IsString()
-  ACCESS_TOKEN_EXPIRES_IN: string
-  @IsString()
-  REFRESH_TOKEN_SECRET: string
-  @IsString()
-  REFRESH_TOKEN_EXPIRES_IN: string
-  @IsString()
-  SECRET_API_KEY: string
-}
-const configServer = plainToInstance(ConfigSchema, process.env, {
-  enableImplicitConversion: true,
+const configSchema = z.object({
+  DATABASE_URL: z.string(),
+  ACCESS_TOKEN_SECRET: z.string(),
+  ACCESS_TOKEN_EXPIRES_IN: z.string(),
+  REFRESH_TOKEN_SECRET: z.string(),
+  REFRESH_TOKEN_EXPIRES_IN: z.string(),
+  SECRET_API_KEY: z.string(),
 })
-const errorArray = validateSync(configServer)
 
-if (errorArray.length > 0) {
-  console.log('Các giá trị khai báo trong file .env không hợp lệ')
-  const errors = errorArray.map((eItem) => {
-    return {
-      property: eItem.property,
-      constraints: eItem.constraints,
-      value: eItem.value,
-    }
-  })
-  throw errors
+const configServer = configSchema.safeParse(process.env)
+
+if (!configServer.success) {
+  console.error(configServer.error)
+  process.exit(1)
 }
-const envConfig = configServer
+const envConfig = configServer.data
 
 export default envConfig
